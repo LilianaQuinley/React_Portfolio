fd595a23cce70ddc400131afef8093282a78206d
liliana Quinley <lilicuellar99@gmail.com>
2021-12-15 22:57:42 -0700

    menu bar is completed and working

---
+++ src/components/portfolioList/PortfolioList.jsx
@@ -0,0 +1,9 @@
+import './portfoliolist.scss'
+
+export default function PortfolioList() {
+  return (
+    <div className='portfoliolist' id='portfoliolist'>
+      
+    </div>
+  )
+}
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,32 @@
+import './topbar.scss'
+import {PhoneIphone, ContactMail} from "@material-ui/icons"
+
+export default function Topbar({menuOpen, setMenuOpen}) {
+  return (
+    <div className = {'topbar ' + (menuOpen && 'active')} >
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className ='logo'>LILI-Q.COM</a>
+          <div className="itemContainer">
+            <PhoneIphone className='icon'/> 
+            <span>(801) 809-7604</span>
+          </div>
+          <div className="itemContainer">
+            <ContactMail className='icon'/>
+            <span>Olga.L.Quinley@gmail.com</span>
+          </div>
+        </div>
+       
+        <div className="right">
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className='line1'></span>
+            <span className='line2'></span>
+            <span className='line3'></span>
+          </div>
+
+          
+        </div>
+      </div>
+    </div>
+  )
+}
